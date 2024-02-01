6d2a77b4822d941e2e9ca8bb8713ca05df90ffb1
Gustavo Canuto <gcanuto@magnasistemas.com.br>
2024-02-01 12:12:15 -0300

    ajuste identação toString Recebido

--- src/br/com/pizzariagustavo/models/Recibo.java
+++ src/br/com/pizzariagustavo/models/Recibo.java
@@ -49,8 +49,8 @@ public class Recibo {
 
 	@Override
 	public String toString() {
-		return "\nRecibo\nidCompra: " + idCompra + "\ncliente: " + cliente + "listaPizzasEscolhidas:"
-				+ listaPizzasEscolhidas + "\nlistaAcompanhamentoEscolhidas: " + listaAcompanhamentoEscolhidas + "\n ";
+		return "\nRecibo\nidCompra: " + idCompra + "\ncliente: " + cliente + "lista Pizzas Compradas:"
+				+ listaPizzasEscolhidas + "\nlista Acompanhamentos Escolhidos: " + listaAcompanhamentoEscolhidas + "\n ";
 	}
 
 
